f802812c281408dc86f56aab9639547f6bd89bea
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-03-07 14:07:27 +0800

    Add tunneling instructions and update layout and index files

--- nativeapp/app/_layout.js
+++ nativeapp/app/_layout.js
@@ -1,8 +1,28 @@
-import {Stack} from "expo-router"
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFont } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
-const Layout = () =>{
-    return <Stack/>
+{
+  /*on Initial Load will make splashScreen Visible */
 }
+SplashScreen.preventAutoHideAsync();
+const Layout = () => {
+  {
+    /*Load The Fonts */
+  }
+  const [fontLoaded] = useFont({
+    DMBold: require("./assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("./assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("./assets/fonts/DMSans-Regular.ttf"),
+  });
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontLoaded]);
+  return <Stack />;
+};
 
-export default Layout
+export default Layout;
---
+++ nativeapp/components/home/welcome/Welcome.jsx
@@ -0,0 +1,35 @@
+import { React, useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { router, useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
+import styles from "./welcome.style";
+
+const Welcome = () => {
+const router = useRouter();
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your Perfect job</Text>
+      </View>
+
+      {/*Another View Container*/}
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+          style={styles.searchInput}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
